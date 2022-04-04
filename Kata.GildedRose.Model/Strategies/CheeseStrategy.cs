f1b3c0b76d05a8cbd282eee18e8d107b7ab533e9
Vdogg <valentintaranto@hotmail.com>
2022-04-04 17:21:52 +0200

    Fixed Thirty days test : Removed non-sense extra code on cheese strategy

--- Kata.GildedRose.Model/Strategies/CheeseStrategy.cs
+++ Kata.GildedRose.Model/Strategies/CheeseStrategy.cs
@@ -11,11 +11,7 @@ namespace Kata.GildedRose.Model.Strategies
             item.SellIn = item.SellIn - 1;
 
             item.Quality = item.SellIn >= 0 ?
-                item.Quality <= 0 ?
-                    QualityUpdater.MIN_QUALITY : item.Quality + 1
-            :
-                item.Quality <= 1 ?
-                    QualityUpdater.MIN_QUALITY : item.Quality + 2;
+                    item.Quality + 1 : item.Quality + 2;
 
             if (item.Quality > QualityUpdater.MAX_QUALITY) item.Quality = QualityUpdater.MAX_QUALITY;
 

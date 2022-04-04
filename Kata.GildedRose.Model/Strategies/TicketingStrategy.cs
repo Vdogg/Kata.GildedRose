af8774afa7759a787fe3897982332c77e62178dc
Vdogg <valentintaranto@hotmail.com>
2022-04-04 16:05:37 +0200

    Fixed Test : Backstage pass increases in Quality as its SellIn value approaches; Added ticketing strategy process. It's the same behaviour as the Cheeses. So it's basically a copypasta.

--- Kata.GildedRose.Model/Strategies/TicketingStrategy.cs
+++ Kata.GildedRose.Model/Strategies/TicketingStrategy.cs
@@ -8,6 +8,16 @@ namespace Kata.GildedRose.Model.Strategies
     {
         public Item Update(Item item)
         {
+            item.SellIn = item.SellIn - 1;
+
+            item.Quality = item.SellIn >= 0 ?
+                item.Quality <= 0 ?
+                    QualityUpdater.MIN_QUALITY : item.Quality + 1
+            :
+                item.Quality <= 1 ?
+                    QualityUpdater.MIN_QUALITY : item.Quality + 2;
+
+            if (item.Quality > QualityUpdater.MAX_QUALITY) item.Quality = QualityUpdater.MAX_QUALITY;
             return item;
         }
     }

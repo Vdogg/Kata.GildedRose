5842ab8a8d74d69207966bfbd6664cd48b11af2e
Vdogg <valentintaranto@hotmail.com>
2022-04-04 13:54:27 +0200

    Ajoutez des fichiers projet.

---
+++ Kata.GildedRose.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata.GildedRose.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

7fc4d9685de63578844e8c94a1d18ce5cc283a05
Vdogg <valentintaranto@hotmail.com>
2022-04-04 13:59:52 +0200

    Added mandatory Item class

---
+++ Kata.GildedRose.Model/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.GildedRose.Model
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + ", " + this.SellIn + ", " + this.Quality;
+        }
+    }
+}
+}

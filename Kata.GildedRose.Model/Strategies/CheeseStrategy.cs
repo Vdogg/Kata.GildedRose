86753ce0bde2fbf91dfcc5a3708faf678091b9b4
Vdogg <valentintaranto@hotmail.com>
2022-04-04 15:32:57 +0200

    Fixed Test : Cheese increases in Quality the older it gets

--- Kata.GildedRose.Model/Strategies/CheeseStrategy.cs
+++ Kata.GildedRose.Model/Strategies/CheeseStrategy.cs
@@ -8,6 +8,15 @@ namespace Kata.GildedRose.Model.Strategies
     {
         public Item Update(Item item)
         {
+            item.SellIn = item.SellIn - 1;
+
+            item.Quality = item.SellIn >= 0 ?
+                item.Quality <= 0 ?
+                    0 : item.Quality + 1
+            :
+                item.Quality <= 1 ?
+                    0 : item.Quality + 2;
+
             return item;
         }
     }
